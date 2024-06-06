100eacd26aa8b93b18bd78406889114026f7a656
a-honey <jungahyeon0512@khu.ac.kr>
2024-06-07 01:40:09 +0900

    feat: 환경 변수 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
 import { ReviewModule } from './modules/review/review.modules';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import typeOrmConfig from 'typeorm.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'ahoney',
-      password: '1234',
-      database: 'vegan-begun',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-    ReviewModule,
-  ],
+  imports: [typeOrmConfig, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const typeOrmConfig = TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+});
+
+export default typeOrmConfig;
