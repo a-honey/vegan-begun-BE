78f794fa47cbd4885a8039dfd3892ccde688a21c
a-honey <jungahyeon0512@khu.ac.kr>
2024-07-22 21:01:28 +0900

    :sparkles: Feat: mySQL 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
 import { ReviewModule } from './modules/review/review.modules';
 import typeOrmConfig from 'typeorm.config';
 
 @Module({
   imports: [typeOrmConfig, ReviewModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- typeorm.config.ts
+++ typeorm.config.ts
@@ -1,12 +1,12 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 const typeOrmConfig = TypeOrmModule.forRoot({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  type: 'mysql',
+  host: '127.0.0.1',
+  port: 3306,
+  username: 'root',
+  password: '1234',
+  database: 'vegan_begun',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true,
 });
